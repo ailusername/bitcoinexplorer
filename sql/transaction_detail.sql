f9c4696b98f0846125b57f40fc47ff1a882b0756
ZhaoFei <946454253@qq.com>
2019-06-14 20:18:20 +0800

    get sql api

---
+++ sql/transaction_detail.sql
@@ -0,0 +1,16 @@
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for transaction_detail
+-- ----------------------------
+DROP TABLE IF EXISTS `transaction_detail`;
+CREATE TABLE `transaction_detail` (
+  `tx_detail_id` bigint NOT NULL auto_increment,
+  `address` VARCHAR (100),
+  `amount` double,
+  `type` tinyint,
+  `txhash` char(64),
+  PRIMARY KEY (`tx_detail_id`),
+  index `idx_address` (`address`),
+  index `idx_txhash` (`txhash`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
