2d1c74efb1d7a5cef55d7d36972e23d35f375267
ZhaoFei <946454253@qq.com>
2019-06-20 15:25:50 +0800

    add vue bitcoinexplorerfount

--- src/main/java/com/ali/bitcoinexplorer/controller/BlockController.java
+++ src/main/java/com/ali/bitcoinexplorer/controller/BlockController.java
@@ -4,10 +4,8 @@ import com.ali.bitcoinexplorer.dto.BlockGetDTO;
 import com.ali.bitcoinexplorer.dto.BlockListDTO;
 import com.ali.bitcoinexplorer.service.BlockService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -15,6 +13,8 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/block")
+@EnableAutoConfiguration
+@CrossOrigin
 public class BlockController {
 
     @Autowired
--- src/main/java/com/ali/bitcoinexplorer/controller/TransactionController.java
+++ src/main/java/com/ali/bitcoinexplorer/controller/TransactionController.java
@@ -69,6 +69,4 @@ public class TransactionController {
         });
         return transactions;
     }
-
-
 }
